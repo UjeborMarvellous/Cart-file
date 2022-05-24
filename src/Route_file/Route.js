f8c62aacc09645b5da422b85273da84e5577e32a
EngrEric <34476840+EngrEric@users.noreply.github.com>
2022-05-24 07:54:13 +0100

    Update Route.js

--- src/Route_file/Route.js
+++ src/Route_file/Route.js
@@ -7,6 +7,7 @@ const Routes = () => {
     return ( 
         <Router>
             <Switch>
+         <Route exact path='/' component={<h1> this is a root component </h1>}/>
                 <Route exact path='/form' component={Form}/>
             </Switch>
         </Router>
